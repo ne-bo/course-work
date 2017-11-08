add another sampling + add L2 by Lera instead of batch normalization at the end of net + fix bug in recall_at_k

cifar.py
@@ -5,7 +5,8 @@ from torch.utils.data.sampler import BatchSampler
 import torchvision.datasets as datasets
 import numpy as np
 from sklearn import model_selection
-from sampling import SmartSampler
+from sampling import PercentageSampler, UniformSampler
+
 
 # I use this class to create a dataset which consist of CIFAR100 splited on train and test such that
 # - train contains images of the first 50 classes
@@ -84,7 +85,6 @@ def create_transformations_for_test_and_train():
     return transform_test, transform_train
 
 
-
 def download_CIFAR100_for_classification():
     transform = create_transformation_for_new_dataset()
     new_test_dataset, new_train_dataset, test, train = create_new_train_and_test_datasets(transform)
@@ -119,13 +119,13 @@ def download_CIFAR100_for_representation():
     new_test_dataset, new_train_dataset, test, train = create_new_train_and_test_datasets(transform)
 
     train_loader = data.DataLoader(new_train_dataset,
-                                   #batch_size=params.batch_size,
-                                   batch_sampler=BatchSampler(sampler=SmartSampler(new_train_dataset,
-                                                                                   batch_size=params.batch_size,
-                                                                                   percentage=5),
+                                   batch_sampler=BatchSampler(
+                                       sampler=UniformSampler(new_train_dataset,
                                                               batch_size=params.batch_size,
-                                                              drop_last=True),
-                                   #shuffle=True,
+                                                              number_of_samples_with_the_same_label_in_the_batch=
+                                                              params.number_of_samples_with_the_same_label_in_the_batch),
+                                       batch_size=params.batch_size,
+                                       drop_last=True),
                                    num_workers=2)
     print('train_loader.batch_size = ', train_loader.batch_size,
           ' train_loader.batch_sampler.batch_size =', train_loader.batch_sampler.batch_size,

histogramm_loss.py
@@ -3,6 +3,7 @@
 
 import torch
 from torch.autograd import Variable
+import numpy as np
 
 
 class HistogramLoss(torch.nn.Module):
@@ -16,6 +17,8 @@ class HistogramLoss(torch.nn.Module):
             self.t = self.t.cuda()
 
     def forward(self, features, classes):
+        #print('features ', features)
+        #print('classes ', np.sort(classes.data.cpu().numpy()))
         def histogram(inds, size):
             s_repeat_ = s_repeat.clone()
             indsa = (delta_repeat == (self.t - self.step)) & inds
@@ -23,12 +26,12 @@ class HistogramLoss(torch.nn.Module):
             s_repeat_[~(indsb | indsa)] = 0
             s_repeat_[indsa] = (s_repeat_ - Variable(self.t) + self.step)[indsa] / self.step
             s_repeat_[indsb] = (-s_repeat_ + Variable(self.t) + self.step)[indsb] / self.step
-
             return s_repeat_.sum(1) / size
 
         classes_size = classes.size()[0]
         classes_eq = (classes.repeat(classes_size, 1) == classes.view(-1, 1).repeat(1, classes_size)).data
         dists = torch.mm(features, features.transpose(0, 1))
+        #print('dists = ', dists)
         s_inds = torch.triu(torch.ones(dists.size()), 1).byte()
         if self.use_gpu:
             s_inds = s_inds.cuda()
@@ -36,6 +39,7 @@ class HistogramLoss(torch.nn.Module):
         neg_inds = ~classes_eq[s_inds].repeat(self.tsize, 1)
         pos_size = classes_eq[s_inds].sum()
         neg_size = (~classes_eq[s_inds]).sum()
+
         s = dists[s_inds].view(1, -1)
         s_repeat = s.repeat(self.tsize, 1)
         delta_repeat = (torch.floor((s_repeat.data + 1) / self.step) * self.step - 1).float()
@@ -46,8 +50,12 @@ class HistogramLoss(torch.nn.Module):
         histogram_pos_inds = torch.tril(torch.ones(histogram_pos_repeat.size()), -1).byte()
         if self.use_gpu:
             histogram_pos_inds = histogram_pos_inds.cuda()
+
         histogram_pos_repeat[histogram_pos_inds] = 0
         histogram_pos_cdf = histogram_pos_repeat.sum(0)
+
+        #print('histogram_neg ', histogram_neg )
+        #print('histogram_pos_cdf ', histogram_pos_cdf)
         loss = torch.sum(histogram_neg * histogram_pos_cdf)
 
         return loss

sampling.py
@@ -44,9 +44,9 @@ def shuffle_with_batch_size(array, batch_size):
 
     start_batches_indices = np.arange(start=0, stop=array.shape[0], step=batch_size)
 
-    print('start_batches_indices = ', start_batches_indices)
+    #print('start_batches_indices = ', start_batches_indices)
     np.random.shuffle(start_batches_indices)
-    print('after shuffling start_batches_indices = ', start_batches_indices)
+    #print('after shuffling start_batches_indices = ', start_batches_indices)
     new_array = np.zeros(n, dtype=int)
     j = 0
     for i in start_batches_indices:
@@ -55,7 +55,7 @@ def shuffle_with_batch_size(array, batch_size):
     return new_array
 
 
-class SmartSampler(Sampler):
+class PercentageSampler(Sampler):
     """Samples elements with majority of samples with the same label
     Arguments:
         percentage (int) : percentage of samples with the same label among all sampled elements
@@ -104,6 +104,82 @@ class SmartSampler(Sampler):
         return self.batch_size
 
 
+class UniformSampler(Sampler):
+    """Samples elements with roughly uniform distribution of samples with the same label
+    Arguments:
+        percentage (int) : percentage of samples with the same label among all sampled elements
+    """
+
+    def __init__(self, data_source, batch_size, number_of_samples_with_the_same_label_in_the_batch):
+        super().__init__(data_source)
+        self.data_source = data_source
+        self.num_samples = len(self.data_source)
+        self.batch_size = batch_size
+        self.number_of_samples_with_the_same_label_in_the_batch = number_of_samples_with_the_same_label_in_the_batch
+
+    def get_indices_for_new_batch(self, remaining):
+        new_batch = np.empty(0, dtype=int)
+
+        all_remaining_labels = np.array(list(set(remaining)))
+        np.random.shuffle(all_remaining_labels)
+
+        while new_batch.shape[0] < self.batch_size:
+            # get new label
+            label = all_remaining_labels[0]
+            #print('label = ', label)
+            all_remaining_labels = all_remaining_labels[1:]
+            np.random.shuffle(all_remaining_labels)
+            # get number of samples for current label in this batch
+            number_of_sumples = np.random.randint(low=int(self.number_of_samples_with_the_same_label_in_the_batch * 0.75),
+                                                  high=int(self.number_of_samples_with_the_same_label_in_the_batch * 1.25))
+            #print('number_of_sumples = ', number_of_sumples)
+            # we will put this indices in our new batch
+            indices_to_put_in_new_batch = np.random.permutation(np.where(remaining == label)[0])[:number_of_sumples]
+            new_batch = np.hstack((new_batch, indices_to_put_in_new_batch))
+            # and remove them from the remaining part
+            #print('puted ', remaining[indices_to_put_in_new_batch].shape)
+
+
+        # and remove the tail from the batch
+        new_batch = new_batch[:self.batch_size]
+        #print('new_batch ', np.sort(remaining[new_batch]), ' new_batch.shape ', new_batch.shape)
+        #print('remaining ', remaining.shape)
+
+        return new_batch, remaining
+
+    # here we stacks arrays of batches with different main labels
+    def __iter__(self):
+        train_labels = np.array(self.data_source.train_labels)
+
+        indices_to_take = np.empty(0, dtype=int)
+        remaining = train_labels
+        number_of_batches = 0
+        while number_of_batches <= len(train_labels)// self.batch_size: #len(remaining) > self.batch_size:
+            new_batch, remaining = self.get_indices_for_new_batch(remaining)
+            # add new indices to all
+            indices_to_take = np.hstack(
+                (indices_to_take,
+                 new_batch))
+            number_of_batches = number_of_batches + 1
+
+        print('indices_to_take = ', indices_to_take.shape, ' ', indices_to_take)
+      #  print('labels to take = ', self.data_source.train_labels[indices_to_take])
+
+        shuffled_batches = shuffle_with_batch_size(indices_to_take, self.batch_size)
+
+        return iter(shuffled_batches)
+
+
+
+    def __len__(self):
+        return self.batch_size
+
+
+
+
+
+
+
 def test_sample():
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.5, 0.5, 0.5),
@@ -115,7 +191,7 @@ def test_sample():
                               transform=transform)
     train_loader = data.DataLoader(train,
                                    batch_size=params.batch_size,
-                                   sampler=SmartSampler(train, batch_size=params.batch_size, percentage=50),
+                                   sampler=PercentageSampler(train, batch_size=params.batch_size, percentage=50),
                                    num_workers=2)
     for i, d in enumerate(train_loader, 0):
         # get the inputs

small_resnet_for_cifar.py
@@ -7,6 +7,19 @@ import torch.optim as optim
 import learning
 import utils
 from torch.optim import lr_scheduler
+import torch
+
+# Lera's implementation
+class L2Normalization(nn.Module):
+    def __init__(self):
+        super(L2Normalization, self).__init__()
+
+    def forward(self, input):
+        input = input.squeeze()
+        return input.div(torch.norm(input, dim=1).view(-1, 1))
+
+    def __repr__(self):
+        return self.__class__.__name__
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -53,7 +66,6 @@ class BasicBlock(nn.Module):
 
 class SmallResnet(nn.Module):
     def __init__(self, block, layers, num_classes):
-        #print('inside network init')
         self.inplanes = 16
         super(SmallResnet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3,
@@ -64,7 +76,6 @@ class SmallResnet(nn.Module):
                                bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        #self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1) #?????
 
         self.layer1 = self._make_layer(block=block, planes=16, blocks=layers[0])
         self.layer2 = self._make_layer(block=block, planes=32, blocks=layers[1], stride=2)
@@ -72,7 +83,8 @@ class SmallResnet(nn.Module):
 
         self.avgpool = nn.AvgPool2d(kernel_size=8)
         self.fc = nn.Linear(64 * block.expansion, num_classes)
-        self.last_bn = nn.BatchNorm1d(num_classes)
+
+        self.norm = L2Normalization()
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -108,7 +120,6 @@ class SmallResnet(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        #x = self.maxpool(x)#????
 
         x = self.layer1(x)
         x = self.layer2(x)
@@ -117,7 +128,9 @@ class SmallResnet(nn.Module):
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
-        x = self.last_bn(x)# added in order to make histogramm loss work
+
+        x = self.norm(x)
+        #x = self.norm(x) # added in order to make histogramm loss work
 
         return x
 

test.py
@@ -2,7 +2,7 @@ from torch.autograd import Variable
 import torch
 from sklearn.neighbors import NearestNeighbors
 import datetime
-
+import numpy as np
 
 def test_for_classification(test_loader, network):
     correct = 0
@@ -20,17 +20,13 @@ def test_for_classification(test_loader, network):
 
 
 def fraction_of_correct_labels_in_array(actual_label, array, labels):
-    correct_labels = 0
-    k = array.shape[0]
-    for i in range(k):
-        if labels[array[i]] == actual_label:
-            correct_labels = correct_labels + 1
-    return float(correct_labels) / float(k)
+    correct_labels = len(np.where(labels.cpu().numpy()[array] == actual_label)[0])
+    return float(correct_labels) / float(array.shape[0])
 
 
 def test_for_representation(test_loader, network, k):
     total_fraction_of_correct_labels = 0
-    total_number_of_outputs = 0
+    total_number_of_batches = 0
 
     for data in test_loader:
         images, labels = data
@@ -41,16 +37,26 @@ def test_for_representation(test_loader, network, k):
         neigh.fit(outputs.data.cpu().numpy())
         neighbors_list = neigh.kneighbors(outputs.data.cpu().numpy(), return_distance=False)
         number_of_outputs = outputs.data.shape[0]
+        total_fraction_of_correct_labels_in_the_batch = 0
         for i in range(number_of_outputs):
             actual_label = labels[i]
+            #print('actual_label', actual_label)
+            #print('neighbors_list[i]', neighbors_list[i])
 
             fraction_of_correct_labels_among_the_k_nearest_neighbors = \
                 fraction_of_correct_labels_in_array(actual_label, neighbors_list[i], labels)
-            total_fraction_of_correct_labels = total_fraction_of_correct_labels + \
-                                               fraction_of_correct_labels_among_the_k_nearest_neighbors
-            total_number_of_outputs = total_number_of_outputs + number_of_outputs
 
-    recall_at_k = float(total_fraction_of_correct_labels) / float(total_number_of_outputs)
-    print('recall_at_k of the network on the ', total_number_of_outputs, ' images: %f ' % recall_at_k)
+            #print('fraction_of_correct_labels_among_the_k_nearest_neighbors ', fraction_of_correct_labels_among_the_k_nearest_neighbors)
+            total_fraction_of_correct_labels_in_the_batch = total_fraction_of_correct_labels_in_the_batch + \
+                                               fraction_of_correct_labels_among_the_k_nearest_neighbors
+        total_number_of_batches = total_number_of_batches + 1
+
+        #print('total_fraction_of_correct_labels_in_the_batch = ', total_fraction_of_correct_labels_in_the_batch)
+        #print('number_of_outputs ', number_of_outputs)
+        fraction_for_this_batch = total_fraction_of_correct_labels_in_the_batch/float(number_of_outputs)
+        total_fraction_of_correct_labels = total_fraction_of_correct_labels + fraction_for_this_batch
+
+    recall_at_k = float(total_fraction_of_correct_labels) / float(total_number_of_batches)
+    print('recall_at_', k, ' of the network on the ', total_number_of_batches, ' batches: %f ' % recall_at_k)
 
     return recall_at_k